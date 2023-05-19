7b5a1995a404bf7c3f37efa29707dd996b3fc198
Ricki <616338459@qq.com>
2023-05-19 09:47:46 +0800

    fix: push-deer test

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '娜娜',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU22773Tafxr8SViNO6RYdPUEmPalOQWfoFFR6BX',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -30,18 +30,18 @@ const USER_CONFIG = {
       // push-deer id
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-07',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+        // {
+        //   type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        // },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // {
+        //   type: '生日', name: '李四', year: '1996', date: '09-31',
+        // },
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '在一起纪念日', year: '2020', date: '11-23',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -82,13 +82,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'J99Yv4nek7hmDcn1ME3fHKTA_-BhOJMWzMKZU-4sko0',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ouCLN5z8OVaHCaJZyp5vQiknKlaE',
+      id: 'PDU22773Tafxr8SViNO6RYdPUEmPalOQWfoFFR6BX',
     }
   ],
 
